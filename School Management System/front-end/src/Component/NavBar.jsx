71461d7655b0ee856e5c94eb92a72d6aa0e5c6b1
Sachin Umayangana <sachinumayangana@gmail.com>
2022-10-23 09:53:00 +0530

    Login

---
+++ School Management System/front-end/src/Component/NavBar.jsx
@@ -0,0 +1,33 @@
+
+import { AppBar, Toolbar, styled } from '@mui/material';
+
+import { NavLink } from 'react-router-dom';
+
+
+const Header = styled(AppBar)`
+    background: #111111;
+`;
+    
+const Tabs = styled(NavLink)`
+    color: #FFFFFF;
+    margin-right: 20px;
+    text-decoration: none;
+    font-size: 20px;
+`;
+
+const NavBar = () => {
+    return (
+        <Header position="static">
+            <Toolbar>
+                <Tabs to="/" exact>School</Tabs>
+                <Tabs to="/students" exact>Students</Tabs>
+                <Tabs to="/teachers" exact>Teachers</Tabs>
+                <Tabs to="/books" exact>Books</Tabs>
+                <Tabs to="/labs" exact>Lab Items</Tabs>
+                <Tabs to="/subjects" exact>Subjects</Tabs>
+            </Toolbar>
+        </Header>
+    )
+}
+
+export default NavBar;
